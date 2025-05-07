26bd80061591462374058e730b2f05af69a2de0e
Santiago Alfonso Rodriguez <santiago_alfonsoro@soy.sena.edu.co>
2025-05-06 19:38:31 -0500

    implementación de operadores de asignación e incremento

---
+++ src/IncrementOperator.java
@@ -0,0 +1,13 @@
+public class IncrementOperator {
+    public static void main(String[] args) {
+        int contador = 0;
+
+        System.out.println("Valor inicial: " + contador);
+        contador++;
+        System.out.println("Después de contador++: " + contador);
+
+        ++contador;
+        System.out.println("Después de ++contador: " + contador);
+
+    }
+}
